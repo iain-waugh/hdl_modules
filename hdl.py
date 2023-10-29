5e459293f470f58cf013f8aaf55450fac62c82eb
Iain Waugh <iwaugh@gmail.com>
2023-10-29 18:44:42 +1100

    First commit of 'hdl' module

---
+++ hdl.py
@@ -0,0 +1,16 @@
+from __future__ import print_function, division
+
+class Module(object):
+    def __init__(self, name):
+        self.name = name
+        return
+
+class Package(object):
+    def __init__(self, name):
+        self.name = name
+        return
+
+class RTL(object):
+    def __init__(self, name):
+        self.name = name
+        return
